79cc236e2137ca41d35d5a82ccb4268dbd8ecea9
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-05 20:42:25 +0100

    Enhance Typographie component by adding alignment options for H1, H2, and Span; update Index screen to include H2 and BtnLink components for improved layout and navigation.

--- components/Typographie.tsx
+++ components/Typographie.tsx
@@ -1,12 +1,28 @@
 import { Text, TextProps } from "react-native"
+import { twMerge } from "tailwind-merge"
 type TypoProps = {
-    children:string
+    children:string,
+    align?:"center" | "left" | "right"
 } & TextProps
 
 
-export const H1 = ({children,...props}:TypoProps)=>{
+export const H1 = ({children,align="left",...props}:TypoProps)=>{
     return (
-        <Text className="text-5xl font-bold" {...props}>
+        <Text className={twMerge("text-5xl font-bold","text-"+align)} {...props}>
+            {children}
+        </Text>
+    )
+}
+export const H2 = ({children,align="left",...props}:TypoProps)=>{
+    return (
+        <Text className={twMerge("text-4xl font-bold","text-"+align)} {...props}>
+            {children}
+        </Text>
+    )
+}
+export const Span = ({children,align="left",...props}:TypoProps)=>{
+    return (
+        <Text className={twMerge("text-2xl","text-"+align)} {...props}>
             {children}
         </Text>
     )
--- screens/auth/index.tsx
+++ screens/auth/index.tsx
@@ -1,14 +1,26 @@
 import Container from 'components/Container'
-import { H1 } from 'components/Typographie'
+import { BtnLink } from 'components/Link'
+import { H1, H2 } from 'components/Typographie'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 const Index = () => {
-  return (
-    <Container className='pt-20'>
-        <H1>Swiplet</H1>
-    </Container>
-  )
+    return (
+        <Container className='py-20 items-center justify-between ' style={{ flex: 1 }}>
+            <H1>Swiplet</H1>
+            <H2 align='center'>
+                Discover and shop unique products with just a swipe.
+            </H2>
+            <View className='w-full gap-4'>
+                <BtnLink screen='Home'>
+                    Already Have one ?
+                </BtnLink>
+                <BtnLink screen='Home'>
+                    Make new account
+                </BtnLink>
+            </View>
+        </Container>
+    )
 }
 
 export default Index
