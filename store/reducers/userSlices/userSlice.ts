9ba2d7708fffe2112293ed05cfea6ae29a68bf1e
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-11 22:25:27 +0100

    feat: define UserType and update initialState in userSlice for better type safety

--- store/reducers/userSlices/userSlice.ts
+++ store/reducers/userSlices/userSlice.ts
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type UserType = {
+    firstName:string;
+    lastName:string;
+    email:string;
+    avatare:string | null
+}
 
-
-const initialState = {
-    avatre:"",
+const initialState: UserType = {
+    avatare:"",
     firstName:"",
     lastName:"",
     email:"",
-}
+} 
 
 
 const useSlice = createSlice({
